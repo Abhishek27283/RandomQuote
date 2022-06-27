4e2c8374d22ae1c0a6ece8fc67c460363241d793
Abhishek27283 <73183253+Abhishek27283@users.noreply.github.com>
2022-06-27 19:28:11 +0530

    Add files via upload

---
+++ build/precache-manifest.63cb46de545d6415a23f016960c35411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e21206684e4f2bad1f4290f6bd6258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2439f4c3b80d9c6d5cbd",
+    "url": "/static/css/main.765b4460.chunk.css"
+  },
+  {
+    "revision": "be8764e90a80b8a08290",
+    "url": "/static/js/2.bf44d81a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.bf44d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2439f4c3b80d9c6d5cbd",
+    "url": "/static/js/main.62949cc1.chunk.js"
+  },
+  {
+    "revision": "54cd695355a35b7373f6",
+    "url": "/static/js/runtime-main.4b623690.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/static/media/city.91c109fc.jpg"
+  }
+]);
